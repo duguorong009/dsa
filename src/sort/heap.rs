f17492cdae8e1e9de54c695b1097df72f4311827
duguorong <dgr93930@gmail.com>
2023-08-28 22:01:16 +0800

    add the "heap_sort" implementation

--- src/sort/heap.rs
+++ src/sort/heap.rs
@@ -19,6 +19,22 @@ fn heapify(arr: &mut Vec<i32>, n: usize, i: usize) {
     }
 }
 
+fn heap_sort(arr: &mut Vec<i32>) {
+    let n = arr.len();
+
+    // build max-heap
+    for i in (0..(n / 2) - 1).rev() {
+        heapify(arr, n, i);
+    }
+
+    for i in (0..n).rev() {
+        arr.swap(0, i);
+
+        // heapify the root element to get highest element at root origin
+        heapify(arr, i, 0);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +46,13 @@ mod tests {
         heapify(&mut arr, n, 0);
 
         assert!(arr == vec![10, 1, 3]);
+    }
+
+    #[test]
+    fn test_heap_sort() {
+        let mut arr = vec![1, 12, 9, 5, 6, 10];
+        heap_sort(&mut arr);
+
+        assert!(arr == vec![1, 5, 6, 9, 10, 12]);
     }
 }
